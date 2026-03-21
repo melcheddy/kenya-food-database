364e0aabb23078bbb4d66fe18e88371e0f2bc5fd
Melcheddy Joshua <meljosh06@gmail.com>
2026-03-21 13:59:28 +0300

    Configure PostgreSQL database

--- nutrition_platform/settings.py
+++ nutrition_platform/settings.py
@@ -69,11 +69,11 @@ WSGI_APPLICATION = 'nutrition_platform.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default='sqlite:///db.sqlite3')
 }
 
 
